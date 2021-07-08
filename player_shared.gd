523d356250803bdd41b7a680156884d839a6bd58
keilaoko <oakley7845@gmail.com>
2021-07-07 20:43:16 -0400

    yea

--- player_shared.gd
+++ player_shared.gd
@@ -12,21 +12,21 @@ var ply_maxlookangle_down = -90
 var ply_maxlookangle_up = 90
 var ply_ylookspeed = 0.3
 var ply_xlookspeed = 0.3
-var ply_sidespeed = 200
-var ply_upspeed = 200
-var ply_forwardspeed = 200
-var ply_backspeed = 200
-var ply_maxspeed = 320
+var ply_sidespeed = 20
+var ply_upspeed = 20
+var ply_forwardspeed = 20
+var ply_backspeed = 20
+var ply_maxspeed = 32
 var ply_accelerate = 20
 var ply_airaccelerate = 20
 var ply_noclipaccelerate = 20
 var ply_airspeedcap = 30
-var ply_friction = 10
+var ply_friction = 4
 var ply_stopspeed = 100
-var ply_gravity = 800
+var ply_gravity = 140
 var ply_maxslopeangle = deg2rad(45)
 var ply_maxvelocity = 35000
-var ply_jumpheight = 54
+var ply_jumpheight = 6
 
 # Cheats
 var noclip : bool
